8953dfa8e2586ffcf4c22f4bf365cfa309e6c5d6
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-27 22:36:20 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,8 +7,8 @@ class Shoe
   def initialize(brand)
     @brand = brand
     BRANDS.each do |existing_brand|
-      if @brand != existing_brand
-           BRANDS << @brand
+      if brand != existing_brand
+           BRANDS << brand
     end
   end
   end
